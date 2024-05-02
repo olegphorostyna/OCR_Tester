d1075735685c22dd62d0404592ab2c2eb809adb7
oforo <olegphorostyna@gmail.com>
2024-05-02 18:20:06 +0300

    Cleanup commit

--- test.py
+++ test.py
@@ -1,7 +1,28 @@
 # Import required packages
 import cv2
 import pytesseract
- 
+import win32api, win32con
+import time
+from PIL import ImageGrab
+import fnmatch
+
+
+def click(x,y):
+    win32api.SetCursorPos((x,y))
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTDOWN,x,y,0,0)
+    win32api.mouse_event(win32con.MOUSEEVENTF_RIGHTUP,x,y,0,0)
+    
+
+
+def takeSnapshot():
+    snapshot = ImageGrab.grab()
+    save_path = "sample.jpg"
+    snapshot.save(save_path)
+    
+
+
+time.sleep(4)
+takeSnapshot() 
 # Mention the installed location of Tesseract-OCR in your system
 # pytesseract.pytesseract.tesseract_cmd = 'System_path_to_tesseract.exe'
  
@@ -29,6 +50,7 @@ dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
 # Finding contours
 contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
                                                  cv2.CHAIN_APPROX_NONE)
+
  
 # Creating a copy of image
 im2 = img.copy()
@@ -44,7 +66,8 @@ file.close()
 # Extracted text is then written into the text file
 for cnt in contours:
     x, y, w, h = cv2.boundingRect(cnt)
-     
+    if(w>100):
+        continue
     # Drawing a rectangle on copied image
     rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 1)
      
@@ -55,8 +78,12 @@ for cnt in contours:
     file = open("recognized.txt", "a")
      
     # Apply OCR on the cropped image
-    text = pytesseract.image_to_string(cropped)
-     
+    text = pytesseract.image_to_string(cropped)  
+    if "Project" in text :
+        print ("Match found: "+str(x+w/2)+" "+str(y+h/2))
+        #Scaling coefficient (e.g. 125%) scrren_x_dim/(img_res*1.25) 
+        click(int(0.8*(x+w/2)),int(0.8*(y+h/2)))
+        click(int(0.8*(x+w/2)),int(0.8*(y+h/2)))
     # Appending the text into file
     file.write(text)
     file.write("\n")
