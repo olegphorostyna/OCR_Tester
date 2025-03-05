50bd42e9ab477585e38be833e90021dbeb3321af
Oleh Forostyna <olegphorostyna@gmail.com>
2025-03-05 17:13:34 +0000

    Context enhanced with rescale feature; sample test started

--- test.py
+++ test.py
@@ -19,12 +19,13 @@ from datetime import datetime
 class Actions(Enum):
     Click = 1
     FailIfExist = 2
+    PassedIfExist = 3 
     
 
 
 # dictionary of the main UI elements (left,top,right,bottom)
 ideUi = {
-    "mainMenu": [4,35,638,70],
+    "mainMenu": [4,38,638,70],
     "mainMenuItems": [4,66,931,619],
     "centrRegion": [600,400, 1700, 800],
     "objectInspector": [256,256,512,512]   
@@ -38,19 +39,24 @@ def rescale(scaleFactor):
 
 class Context: 
     
-    def __init__(self):
+    def __init__(self,scaleFactor,clickScale):
         self.dirPath=os.path.join(os.getcwd(),datetime.now().strftime('%Y-%m-%d_%H-%M-%S'))
         os.makedirs(self.dirPath)
         self.actionCounter=-1
         self.artifactsFolder=""
+        #Scaling coefficient (e.g. 125%) scrren_x_dim/(img_res*1.25)=1/1.25
+        #Regular display Display resolution 1920*966 scale 125 = 0.8
+        #4k(3849*2160) Display resolution 3840*1948 scale 200 = 0.5
+        self.clickScale=clickScale;
         # Originaly ui elements was created for HD monitor with scale=125%
         # With 4K monitor with scale set to 200% we need muliply each dimension by 200/125=1.6 
-        rescale(1.6)
+        rescale(scaleFactor)
     
     def setTest(self,testName): 
         self.actionCounter=-1        
         self.artifactsFolder=os.path.join(self.dirPath, testName)
         os.makedirs(self.artifactsFolder)
+        print(testName)
     
     def getArtifactName(self):
         self.actionCounter+=1
@@ -73,21 +79,14 @@ def clickLeft(x,y):
     win32api.mouse_event(win32con.MOUSEEVENTF_LEFTUP,x,y,0,0) 
 
 
-def takeSnapshot(uiElement):
-    #return ImageGrab.grab(bbox=(0.8*uiElement[0], 0.8*uiElement[1], 0.8*uiElement[2], 0.8*uiElement[3])) #4k no scaling
-    #return ImageGrab.grab(bbox=(1.6*uiElement[0], 1.6*uiElement[1], 1.6*uiElement[2], 1.6*uiElement[3])) #4k 200 scaling
-    return ImageGrab.grab(bbox=(uiElement[0], uiElement[1], uiElement[2], uiElement[3])) #4k 200 scaling with modified ideUi dictionary
+def takeSnapshot(uiElement):    
+    return ImageGrab.grab(bbox=(uiElement[0], uiElement[1], uiElement[2], uiElement[3])) 
     # take whole screen 
     # snapshot = ImageGrab.grab()
     
  
-def clikElement(uiElement, x, y, w, h):
-    #print ("Match found: "+str(int(0.8*(x+uiElement[0]+w/2)))+" "+str(int(0.8*(y+uiElement[1]+h/2))))
-    #Scaling coefficient (e.g. 125%) scrren_x_dim/(img_res*1.25)=1/1.25
-    #Regular display Display resolution 1920*966 scale 125
-    #4k(3849*2160) Display resolution 3840*1948 scale 200
-    #clickLeft(int(0.8*(x+uiElement[0]+w/2)),int(0.8*(y+uiElement[1]+h/2)))  
-    clickLeft(int(0.5*(x+uiElement[0]+w/2)),int(0.5*(y+uiElement[1]+h/2))) #4k click scale 
+def clikElement(uiElement, x, y, w, h,clickScale):     
+    clickLeft(int(clickScale*(x+uiElement[0]+w/2)),int(clickScale*(y+uiElement[1]+h/2))) #4k click scale 
 
 def prepareSnapshot(uiElement, context):
     snapshot = takeSnapshot(uiElement)
@@ -115,53 +114,42 @@ def prepareSnapshot(uiElement, context):
     dilation = cv2.dilate(thresh1, rect_kernel, iterations = 1)
     
     # Finding contours
-    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL, 
-                                                    cv2.CHAIN_APPROX_NONE)
-    
-    
-    # Creating a copy of image
-    im2 = img.copy()
-    
-    # A text file is created and flushed
-    file = open(context.recognized(), "w+")
-    file.write("")
-    file.close()  
-    
-    
-    return contours, im2, file 
+    #contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_NONE)     
+    contours, hierarchy = cv2.findContours(dilation, cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)     
+    return contours, img 
 
-def performAction(action, contours, name, image, uiElement,psm,context):
-    # Open the file in append mode
-    #file = open("recognized.txt", "a")
-    file = open(context.recognized(), "w+")
-    im2 = image.copy()
+def performAction(action, contours, name, image, uiElement,psm,context):    
+    file = open(context.recognized(), "w+")     
     # Looping through the identified contours
     # Then rectangular part is cropped and passed on
     # to pytesseract for extracting text from it
     # Extracted text is then written into the text file
+    debugImage=image.copy();
     for cnt in contours:
         x, y, w, h = cv2.boundingRect(cnt)
         # Drawing a rectangle on copied image
-        rect = cv2.rectangle(im2, (x, y), (x + w, y + h), (0, 255, 0), 1)
+        rect = cv2.rectangle(debugImage, (x, y), (x + w, y + h), (0, 255, 0), 1)
         # Cropping the text block for giving input to OCR
         cropped = image[y:y + h, x:x + w]
-        # Check what we feed into pytesseract
-        # cv2.imwrite('cropped %d.jpg' %x ,cropped)  
+        # Check what we feed into pytesseract <<DEBUG_FUNCTION>>
+        #cv2.imwrite('cropped %d.jpg' %x ,cropped)  
         # Apply OCR on the cropped image
         # page segmentation modes (psm) is set
         text = pytesseract.image_to_string(cropped,lang='eng',config=psm)  
         if name in text : 
             match action:
                 case Actions.Click:
-                    clikElement(uiElement, x, y, w, h)                 
-                    cv2.circle(im2, (x+int(w/2),y+int(h/2)), radius=7, color=(0, 0, 255), thickness=-1)
+                    clikElement(uiElement, x, y, w, h, context.clickScale)                 
+                    cv2.circle(debugImage, (x+int(w/2),y+int(h/2)), radius=7, color=(0, 0, 255), thickness=-1)
                 case Actions.FailIfExist:                          
-                    print("Failed!")                   
+                    print("Failed!")
+                case Actions.PassedIfExist:
+                    print("Passed!")
         if text:
             file.write(text)            
     # Close the file
     file.close
-    cv2.imwrite(context.out(),im2)
+    cv2.imwrite(context.out(),debugImage)
     
 
 #Define all the tests here
@@ -170,23 +158,34 @@ def performAction(action, contours, name, image, uiElement,psm,context):
 # Demonstarete main functionality (contour drawing, element clicking) 
 def sampleTest(context): 
     context.setTest("sampleTest") 
-    contours, im2, file = prepareSnapshot(ideUi["mainMenu"],context)
-    performAction(Actions.Click, contours, "Help", im2, ideUi["mainMenu"],"--psm 8",context)
+    contours, im2 = prepareSnapshot(ideUi["mainMenu"],context)
+    performAction(Actions.Click, contours, "File", im2, ideUi["mainMenu"],"--psm 8",context)
     #restore for a next test
-    performAction(Actions.Click, contours, "Help", im2, ideUi["mainMenu"],"--psm 8",context)
+    performAction(Actions.Click, contours, "File", im2, ideUi["mainMenu"],"--psm 8",context)
     
 #https://embt.atlassian.net/browse/RS-124356
-def chekBitmapStyleDesigner(context):
-    print("chekBitmapStyleDesigner test:")
+def chekBitmapStyleDesigner(context):    
     context.setTest("chekBitmapStyleDesigner")
-    contours, im2, file = prepareSnapshot(ideUi["mainMenu"],context)
+    contours, im2 = prepareSnapshot(ideUi["mainMenu"],context)
     performAction(Actions.Click, contours, "Tools", im2, ideUi["mainMenu"],"--psm 8",context)
     
-    contours, im2, file = prepareSnapshot(ideUi["mainMenuItems"],context)
+    contours, im2 = prepareSnapshot(ideUi["mainMenuItems"],context)
     performAction(Actions.Click, contours, "Bitmap", im2, ideUi["mainMenuItems"], "--psm 7",context)
     
-    contours, im2, file = prepareSnapshot(ideUi["centrRegion"],context)
+    contours, im2 = prepareSnapshot(ideUi["centrRegion"],context)
     performAction(Actions.FailIfExist, contours, "Access", im2, ideUi["centrRegion"], "--psm 11",context)
+
+def createNewVCLProject(context):    
+    context.setTest("createNewVCLProject")
+    contours, im2 = prepareSnapshot(ideUi["mainMenu"],context)
+    performAction(Actions.Click, contours, "File", im2, ideUi["mainMenu"],"--psm 8",context)
+    time.sleep(0.5) 
+    contours, im2 = prepareSnapshot(ideUi["mainMenuItems"],context)
+    performAction(Actions.Click, contours, "New", im2, ideUi["mainMenuItems"], "--psm 7",context)
+    time.sleep(0.5) 
+    contours, im2 = prepareSnapshot(ideUi["mainMenuItems"],context)
+    performAction(Actions.Click, contours, "Windows VCL Application - Delphi", im2, ideUi["mainMenuItems"], "--psm 7",context)
+    
     
     
     
@@ -194,8 +193,10 @@ def chekBitmapStyleDesigner(context):
 # Mention the installed location of Tesseract-OCR in your system
 pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'  
 
-context = Context()   
+#Set display scale and click scale before running test sequence
+context = Context(1.6,0.5)   
 #Test to run:
 time.sleep(4) 
-sampleTest(context)
+#sampleTest(context)
 #chekBitmapStyleDesigner(context)
+createNewVCLProject(context)
